6c7f082be08c2f6f9c9141b441d3b7b0c3e539a9
loganwu <wuxiaolong63@gmail.com>
2014-07-10 10:46:26 +0800

    add crossPlatform complie

--- ConndSvr/makefile.mk
+++ ConndSvr/makefile.mk
@@ -1,4 +1,5 @@
 #####################################################
+PLATFORM=macosx
 
 EXT_LIB_ROOT=${BUILD_ROOT_PATH}/extlib
 
@@ -10,7 +11,11 @@ INC_PROBUFEVENT= -I$(EXT_LIB_ROOT)/protobuf/include/
 INC_EXT =  $(INC_LIBEVENT) $(INC_PROBUFEVENT) 
 
 LIB_MYSQL = -L$(MYSQL_ROOT)/lib/mysql -lmysqlclient -lz
-LIB_EVENT= -L$(EXT_LIB_ROOT)/libevent/lib/ -levent -lrt #libevent depends on librt
+ifeq ($(PLATFORM),macosx)
+	LIB_EVENT= -L$(EXT_LIB_ROOT)/libevent/lib/ -levent 
+else #linux
+	LIB_EVENT= -L$(EXT_LIB_ROOT)/libevent/lib/ -levent -lrt #linux libevent depends on librt
+endif
 LIB_PROBUF= -L$(EXT_LIB_ROOT)/protobuf/lib/ -lprotobuf 
 
 LIB_EXT = $(LIB_EVENT) $(LIB_PROBUF)
