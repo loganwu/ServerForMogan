f95b5e9d049660f437ed7b6f4a04d27c8410c20b
loganwu <wuxiaolong63@gmail.com>
2014-07-10 12:18:53 +0800

    add extlib complie script

---
+++ ConndSvr/protobuf-2.5.0/conv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PWD_PATH=`pwd`
+EXT_LIB_NAME=protobuf
+EXT_LIB_PATH=$PWD_PATH/../extlib/$EXT_LIB_NAME 
+INSTALL_PATH=$PWD_PATH/installDir
+
+function copy_lib
+{
+	cd $INSTALL_PATH 
+	cp lib/lib$EXT_LIB_NAME.a $EXT_LIB_PATH/lib
+	cp -rf include/*  $EXT_LIB_PATH/include
+}
+
+./configure --prefix=$INSTALL_PATH && make && make install
+echo "copy libevent to $EXT_LIB_PATH"
+copy_lib 
+
