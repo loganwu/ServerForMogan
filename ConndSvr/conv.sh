9669e7e6f969ab79b718ffce53d4811e883a043c
loganwu <loganwu@tencent.com>
2014-07-11 17:30:05 +0800

    mod

--- ConndSvr/conv.sh
+++ ConndSvr/conv.sh
@@ -5,7 +5,7 @@ echo "BUILD_ROOT_PATH=`pwd`" > ./BUILD_ROOT
 SCRPT_PATH=`pwd`
 PATH=$PATH:$SCRPT_PATH/bin/
 chmod +x ./bin/*
-PROTO_SRC_PATH=./ConndServerSrc/protocol/
+PROTO_SRC_PATH=./ConndServerSrc/protocol/pb
 PROTO_CLIENT_SRC_PATH=./ClientSrc/protocol/
 function rename
 {
@@ -19,6 +19,7 @@ function rename
 #create protocal tdr file
 function create_protocal_file
 {
+	cd $SCRPT_PATH 
 	cd $PROTO_SRC_PATH && for file in `ls *.proto`;do protoc --cpp_out=. $file;done 
 	#find . -name "*.cc"|xargs rename cc cpp
 	rename
@@ -26,6 +27,7 @@ function create_protocal_file
 
 function create_client_protocal_file
 {
+	cd $SCRPT_PATH 
 	cd $PROTO_CLIENT_SRC_PATH && for file in `ls *.proto`;do protoc --cpp_out=. $file;done 
 	#find . -name "*.cc"|xargs rename cc cpp
 	rename
@@ -37,6 +39,21 @@ function build_ext_lib
 	cd ../protobuf-2.5.0 && ./conv.sh
 }
 
-build_ext_lib
-cd $SCRPT_PATH && create_protocal_file
-cd $SCRPT_PATH && create_client_protocal_file
+
+case "$1" in
+    all)
+		build_ext_lib
+		create_protocal_file
+		create_client_protocal_file
+        ;;
+    lib)
+		build_ext_lib
+        ;;
+    protocal)
+		create_protocal_file
+		create_client_protocal_file
+        ;;
+    *)
+        echo $"Usage: $0 (all|lib|protocal)"
+        ;;
+esac
