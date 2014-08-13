7c273446d96ee6a570c4ae824204405c936ee53b
loganwu <loganwu@tencent.com>
2014-08-13 17:48:32 +0800

    add shmWrapp

---
+++ ConndSvr/ConndServerSrc/comm/ShmqWrapper.cpp
@@ -0,0 +1,15 @@
+#include "ShmqWrapper.h"
+#include "SingletonHolder.h"
+#include "shm_queue.h"
+
+
+ShmqWrapper::ShmqWrapper()
+{
+
+}
+
+ShmqWrapper::~ShmqWrapper()
+{
+
+}
+
---
+++ ConndSvr/ConndServerSrc/comm/ShmqWrapper.h
@@ -0,0 +1,14 @@
+#ifndef  _SHMQWRAPPER_H_
+#define  _SHMQWRAPPER_H_
+
+class ShmqWrapper
+{
+public:
+	ShmqWrapper();
+	~ShmqWrapper();
+private:
+
+
+};
+
+#endif   /* ----- #ifndef _SHMQWRAPPER_H_  ----- */
