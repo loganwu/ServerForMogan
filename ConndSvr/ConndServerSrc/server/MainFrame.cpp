251718c6334e92a94512322092e3f857dd2bafaf
loganwu <loganwu@tencent.com>
2014-08-14 15:40:42 +0800

    m

--- ConndSvr/ConndServerSrc/server/MainFrame.cpp
+++ ConndSvr/ConndServerSrc/server/MainFrame.cpp
@@ -5,12 +5,24 @@
 #include "SingletonHolder.h"
 #include "log.h"
 #include "MemMgr.h"
+#include "ShmqWrapper.h"
+
+struct GameConf
+{
+	int iMemcacheKey;
+	int iSessionKey;
+	int iShmqueKey;
+	GameConf(){memset(this,0,sizeof(*this));}
+};
+
+GameConf g_stConf;
 
 #define _INIT_DEF(ClassName) do \
 { \
 	if(SingletonHolder<ClassName>::Instance()->Init() != 0) \
 	{ \
 		logerr("%s init failure\n",#ClassName); \
+		return -1;\
 	} \
 	else \
 	{ \
@@ -21,9 +33,18 @@
 
 int Init()
 {
+	g_stConf.iShmqueKey = 20140814;
 	_INIT_DEF(NetworkWrapper);
 	_INIT_DEF(MemMgr);
-	
+	if(SingletonHolder<ShmqWrapper>::Instance()->InitProducer(g_stConf.iShmqueKey ) != 0)
+	{
+		logerr("ShmqWrapper init failure\n");
+	}
+	else 
+	{ 
+		logdbg("ShmqWrapper init success[key %d]\n",g_stConf.iShmqueKey); 
+	} 
+
 	return 0;
 }
 
