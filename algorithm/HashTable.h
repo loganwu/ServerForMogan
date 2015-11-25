69519aee668dcdb29d069777c96b5f52f9264e80
loganwu <loganwu@tencent.com>
2015-11-25 17:34:52 +0800

    add hashtable

---
+++ algorithm/HashTable.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ * 
+ *       Filename:  HashTable.h
+ * 
+ *    Description: simple hashtable with fixed buckets reference GNU C++ /usr/include/c++/4.1.2/ext/hashtable.h 
+ * 
+ *        Version:  1.0
+ *        Created:  11/25/2015 05:33:51 PM CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ * 
+ *         Author:  loganwu (linux), loganwu@tencent.com
+ *        Company:  tencent
+ * 
+ * =====================================================================================
+ */
+
+#ifndef  HASHTABLE_INC
+#define  HASHTABLE_INC
+
+#endif   /* ----- #ifndef HASHTABLE_INC  ----- */
+
