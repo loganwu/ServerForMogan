18549fbbc2f188e7daf5b6bd662ca3df3bdf5b8c
loganwu <loganwu@tencent.com>
2015-11-30 19:58:19 +0800

    hashTable done

--- algorithm/HashTable.h
+++ algorithm/HashTable.h
@@ -20,10 +20,12 @@
 #define  HASHTABLE_INC
 #include <utility>
 #include <iterator>
+#include <iostream>
 
 namespace Logan
 {
 	enum { _S_num_primes = 28 };
+	enum{DISABLE = -1};
 	static const size_t stl_prime_list[_S_num_primes] =
 	{
 		53ul,         97ul,         193ul,       389ul,       769ul,
@@ -51,13 +53,18 @@ namespace Logan
 		linktype  next;
 	};
 
-	template <class T,class K>
+	template < class T, class K,int N>
+	class HashTable;
+
+	template <class T,class K,int N>
 	struct HashIterator: public  std::iterator< std::forward_iterator_tag, T >
 	{
-		typedef HashIterator<T,K> self;
+		typedef HashIterator<T,K,N> self;
+		typedef HashTable<T,K,N> hashtable;
+		typedef HashNode<T,K> hashnode;
 
-		HashIterator() : pNode(NULL){}
-		HashIterator(linktype p) : pNode(p){}
+		HashIterator() : pNode(NULL), ht(NULL){}
+		HashIterator(hashnode* p, hashtable* hash) : pNode(p),ht(hash){}
 		~HashIterator()  {}
 
 		T& operator*()
@@ -72,6 +79,23 @@ namespace Logan
 
 		self&  operator++()
 		{
+			hashnode* old = pNode;
+			if(old->next == DISABLE) //next node null
+			{
+				linktype num = ht->bucket_num(old->key);
+				linktype cur = ht->pBuckets[num];
+				for(; cur == DISABLE && num < ht->bucket_size();num++)
+					cur = ht->pBuckets[num];
+				if(DISABLE == cur)
+					pNode = NULL;
+				else
+					pNode = &(ht->vData[cur]);
+				
+			}
+			else
+			{
+				pNode = &(ht->vData[pNode->next]);
+			}
 			return *this;
 		}
 
@@ -82,7 +106,8 @@ namespace Logan
 			return tmp;
 		}
 
-		HashNode<T,K>* pNode;
+		hashnode* pNode;
+		hashtable* ht;
 	};
 	
 	// data type key type data size
@@ -90,28 +115,124 @@ namespace Logan
 	class HashTable
 	{
 	public:
-		typedef HashIterator<T,K> iterator;
+		enum{HASH_EXIST= -2, HASH_FULL = -1,HASH_SUCC};
+		typedef HashIterator<T,K,N> iterator;
 		HashTable(){init();}	
 		~HashTable(){if(NULL == pBuckets) delete[] pBuckets;}	
 		
+		linktype bucket_num(K key){return (linktype)(key%N);};
+		size_t bucket_size(){return next_prime_size(N);};
+		size_t count(){return eleCount;};
 		std::pair< iterator,int> insert(const K& k, const T& t);
 		int erase(const K& k);
-		void clear();
+		T* get(const K& k);
+		//void clear();
+
 	private:
 		
 		void init()
 		{
 			memset(this,0,sizeof(*this));
 			pBuckets = new linktype[next_prime_size(N)];
+			for(int i =0;i < N;i++)
+				vData[i].next = i+1;
+				vData[N-1].next = DISABLE;
+			
+			for(int i =0;i < bucket_size();i++)
+				pBuckets[i] = DISABLE;
+
 		}
-	private:
+	public:
 		linktype* pBuckets;	
 		linktype free;
 		HashNode<T,K> vData[N];
+	private:
+		size_t eleCount;
 
 		
 	};// HashTable class
 
+	template<class T, class K, int N>
+	std::pair< typename HashTable<T,K,N>::iterator,int> HashTable<T,K,N>::insert(const K& k, const T& t)
+	{
+		iterator it;
+		if(DISABLE == free || eleCount > N)
+		{
+			std::cout<<"ele full "<<k<<std::endl;
+			return std::make_pair(it,-1);
+		}
+
+		linktype num = bucket_num(k);
+		linktype cur = pBuckets[num];
+		while( DISABLE != cur)	
+		{
+			if(vData[cur].key == k)
+			{
+				std::cout<<"replicated key "<<k<<std::endl;
+				return std::make_pair(it,-2);
+			}
+			cur = vData[cur].next;
+		}
+
+		vData[free].value = t;
+		vData[free].key= k;
+		linktype freeNext = vData[free].next;
+		vData[free].next = pBuckets[num];
+		pBuckets[num] = free;	
+		
+
+		//update free 
+		eleCount++;
+		free = freeNext;
+		iterator it1( &(vData[free]),this);
+
+		return std::make_pair(it1,0);
+	}
+
+	template<class T, class K, int N>
+	int HashTable<T,K,N>::erase(const K& k )
+	{
+		linktype num = bucket_num(k);
+		linktype cur = pBuckets[num];
+		linktype prev = cur; 
+		while( DISABLE != cur)	
+		{
+			if(vData[cur].key == k)
+			{
+				std::cout<<"erase key "<<k<<std::endl;
+				pBuckets[num] = vData[cur].next;
+				vData[cur].next = free;
+				free = cur;
+				vData[prev].next = vData[cur].next;
+				
+				eleCount--;
+
+				return 0;
+			}
+			prev = cur;
+			cur = vData[cur].next;
+		}
+
+		return -1;
+	}
+
+	template<class T, class K, int N>
+	T* HashTable<T,K,N>::get(const K& k )
+	{
+		linktype num = bucket_num(k);
+		linktype cur = pBuckets[num];
+		while( DISABLE != cur)	
+		{
+			if(vData[cur].key == k)
+			{
+				std::cout<<"get key "<<k<<std::endl;
+				return &(vData[cur].value);
+			}
+			cur = vData[cur].next;
+		}
+
+		return NULL;
+	}
 }
 
 #endif   /* ----- #ifndef HASHTABLE_INC  ----- */
--- algorithm/hashTblExp.cpp
+++ algorithm/hashTblExp.cpp
@@ -21,9 +21,30 @@
 #include <algorithm>
 #include <stdint.h>
 using namespace std;
+struct Hero
+{
+	char name[10];
+	uint64_t ullHeroId;
+};
 int main()
 {
 
-	Logan::HashTable<int, uint64_t,100> cache;
+	Logan::HashTable<Hero, uint64_t,100> cache;
+	typedef Logan::HashTable<Hero, uint64_t,100> hashtype;
+	Hero h1;
+	hashtype::iterator it;
+	h1.ullHeroId = 123;
+	cout<<"ele count "<<cache.count()<<endl;
+	cout<<"buckets count "<<cache.bucket_size()<<endl;
+	pair<hashtype::iterator,int> ret = cache.insert(h1.ullHeroId,h1);
+	cout<<"ele count "<<cache.count()<<endl;
+	pair<hashtype::iterator,int> ret1 = cache.insert(h1.ullHeroId,h1);
+	cout<<"ele count "<<cache.count()<<endl;
+	Hero* pHero = cache.get(123);
+	cout<<"hero heroid "<<pHero->ullHeroId<<endl;
+	cache.erase(123);
+	 pHero = cache.get(123);
+	cout<<"after erase ele count "<<cache.count()<<" heroid 123 exists: "<<(pHero!=NULL)<<endl;
+	
 	return 0;
 }
