cac6443cc4dfd4792af57f134028eaae976b829c
loganwu <loganwu@tencent.com>
2015-11-30 13:26:30 +0800

    m hash

--- algorithm/HashTable.h
+++ algorithm/HashTable.h
@@ -23,30 +23,49 @@
 
 namespace Logan
 {
+	enum { _S_num_primes = 28 };
+	static const size_t stl_prime_list[_S_num_primes] =
+	{
+		53ul,         97ul,         193ul,       389ul,       769ul,
+		1543ul,       3079ul,       6151ul,      12289ul,     24593ul,
+		49157ul,      98317ul,      196613ul,    393241ul,    786433ul,
+		1572869ul,    3145739ul,    6291469ul,   12582917ul,  25165843ul,
+		50331653ul,   100663319ul,  201326611ul, 402653189ul, 805306457ul,
+		1610612741ul, 3221225473ul, 4294967291ul
+	};
+
+	size_t next_prime_size(size_t n)
+	{
+		const size_t* first = stl_prime_list;
+		const size_t* last = stl_prime_list + (int)_S_num_primes;
+		const size_t* pos = std::lower_bound(first, last, n);
+		return pos == last ? *(last - 1) : *pos;
+	}
+	typedef size_t linktype;
+
 	template <class T, class K >
 	struct HashNode
 	{
 		T value;
 		K key;
-		HashNode<T>* next;
+		linktype  next;
 	};
 
-	template <class T,K>
+	template <class T,class K>
 	struct HashIterator: public  std::iterator< std::forward_iterator_tag, T >
 	{
 		typedef HashIterator<T,K> self;
-		typedef HashNode<T,K>* linktype;
 
 		HashIterator() : pNode(NULL){}
 		HashIterator(linktype p) : pNode(p){}
-		~HashIterator() : {}
+		~HashIterator()  {}
 
-		reference operator*()
+		T& operator*()
 		{
 			return pNode->value;
 		}
 
-		pointer operator->()
+		T* operator->()
 		{
 			return &(operator*());
 		}
@@ -63,7 +82,7 @@ namespace Logan
 			return tmp;
 		}
 
-		linktype  pNode;
+		HashNode<T,K>* pNode;
 	};
 	
 	// data type key type data size
@@ -72,35 +91,23 @@ namespace Logan
 	{
 	public:
 		typedef HashIterator<T,K> iterator;
-		typedef size_t nodeIndex;
-		HashTable(){}	
-		~HashTable(){}	
+		HashTable(){init();}	
+		~HashTable(){if(NULL == pBuckets) delete[] pBuckets;}	
 		
-		pair< iterator,int> insert(const K& k, const T& t);
+		std::pair< iterator,int> insert(const K& k, const T& t);
 		int erase(const K& k);
+		void clear();
 	private:
 		
-		enum { _S_num_primes = 28 };
-		static const size_t __stl_prime_list[_S_num_primes] =
+		void init()
 		{
-			53ul,         97ul,         193ul,       389ul,       769ul,
-			1543ul,       3079ul,       6151ul,      12289ul,     24593ul,
-			49157ul,      98317ul,      196613ul,    393241ul,    786433ul,
-			1572869ul,    3145739ul,    6291469ul,   12582917ul,  25165843ul,
-			50331653ul,   100663319ul,  201326611ul, 402653189ul, 805306457ul,
-			1610612741ul, 3221225473ul, 4294967291ul
-		};
-
-		size_t next_prime_size(size_t n);
-		{
-			const size_t* __first = __stl_prime_list;
-			const size_t* __last = __stl_prime_list + (int)_S_num_primes;
-			const size_t* pos = std::lower_bound(__first, __last, __n);
-			return pos == __last ? *(__last - 1) : *pos;
+			memset(this,0,sizeof(*this));
+			pBuckets = new linktype[next_prime_size(N)];
 		}
 	private:
-		const bucket_size = next_prime_size(N); 
-		nodeIndex buckets[bucket_size];
+		linktype* pBuckets;	
+		linktype free;
+		HashNode<T,K> vData[N];
 
 		
 	};// HashTable class
---
+++ algorithm/hashTblExp.cpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hashTblExp.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/26/2015 03:14:02 PM CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  loganwu (linux), loganwu@tencent.com
+ *        Company:  tencent
+ *
+ * =====================================================================================
+ */
+
+#include "HashTable.h"
+#include <iostream>
+#include <algorithm>
+#include <stdint.h>
+using namespace std;
+int main()
+{
+
+	Logan::HashTable<int, uint64_t,100> cache;
+	return 0;
+}
